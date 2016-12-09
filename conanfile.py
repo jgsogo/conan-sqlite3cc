4557d47bfe519bc57589c799d12d97b77e24e3b9
Javier G. Sogo <jgsogo@gmail.com>
2016-12-09 13:05:21 +0100

    call exe

--- conanfile.py
+++ conanfile.py
@@ -44,7 +44,10 @@ class SQLite3ccConan(ConanFile):
         self.run(build_command)
         
         # Run tests, always
-        command = "cd {} && ./tests".format(os.path.join(self._build_dir, "bin"))
+        if self.settings.os == "Windows":
+            command = "cd {} && ./tests.exe".format(os.path.join(self._build_dir, "bin"))
+        else:
+            command = "cd {} && ./tests".format(os.path.join(self._build_dir, "bin"))
         self.output.info(command)
         self.run(command)
 
