12c8d69f007c500b3f0dfcced773f77b77f9311f
Javier G. Sogo <jgsogo@gmail.com>
2016-12-11 17:55:23 +0100

    install all in one line

--- .travis/install.sh
+++ .travis/install.sh
@@ -19,7 +19,5 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-pip install conan_package_tools # It install conan too
+pip install bzr conan_package_tools # It install conan too
 conan user
-pip install bzr
-
